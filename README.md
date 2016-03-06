00965b4c1103be6e0ddb9c619530713a24c4e7c5
raul <raulsenaferreira@gmail.com>
2016-03-06 12:26:34 -0300

    updating my portfolio

--- README.md
+++
@@ -1,4 +0,0 @@
-Portfolio
-=========
-
-Site criado para mostrar meu portfolio
---
+++ mail/contact_me.php
@@ -0,0 +1,49 @@
+<?php
+// Pear Mail Library
+require_once "Mail.php";
+
+// Check for empty fields
+if(empty($_POST['name'])  		||
+   empty($_POST['email']) 		||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+	
+$name = $_POST['name'];
+$email_address = $_POST['email'];
+$message = $_POST['message'];
+	
+// Create the email and send the message
+
+
+$from = $email_address;
+$to = 'raulsenaferreira@gmail.com';
+$subject = $message;
+$body = $name;
+
+$headers = array(
+    'From' => $from,
+    'To' => $to,
+    'Subject' => $subject
+);
+
+$smtp = Mail::factory('smtp', array(
+        'host' => 'ssl://smtp.gmail.com',
+        'port' => '587',
+        'auth' => true,
+        'username' => 'raulsenaferreira@gmail.com',
+        'password' => 'raul0128raul'
+    ));
+
+$mail = $smtp->send($to, $headers, $body);
+
+if (PEAR::isError($mail)) {
+    echo('<p>' . $mail->getMessage() . '</p>');
+} else {
+    echo('<p>Message successfully sent!</p>');
+}
+return true;			
+?>
